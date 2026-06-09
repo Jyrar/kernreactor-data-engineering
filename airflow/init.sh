76d42b76730a2b49a47589d98b93e6377842752b
Erik Voogd <evoogd@quintor.nl>
2026-06-09 11:22:43 +0200

    docker compose with airflow

---
+++ airflow/init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+airflow db migrate
+
+airflow users create \
+  --username admin \
+  --password admin \
+  --firstname Admin \
+  --lastname User \
+  --role Admin \
+  --email admin@example.com \
+  || true
+
+airflow webserver &
+exec airflow scheduler
